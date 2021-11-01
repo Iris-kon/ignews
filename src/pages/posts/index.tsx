9193fdedb81e7dd65f96769304d87d4d561c7706
Thiago Oliveira <thiagooliveira.k.on@gmail.com>
2021-10-31 23:57:52 -0300

    create posts routes and style it

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,32 @@
+import Head from 'next/head'
+import styles from './styles.module.scss'
+
+export default function Posts() {
+    return (
+        <>
+            <Head>
+                <title>Post | Ignews</title>
+            </Head>
+
+            <main className={styles.container}>
+                <div className={styles.posts}>
+                    <a href="#">
+                        <time>12 de março de 2021</time>
+                        <strong>Creating a Monorepo with Lerna & Yarn workspaces</strong>
+                        <p>In this guide, tou will learn how to create a Monorepo to manage multiples packages with a shared build, test, and release process</p>
+                    </a>
+                    <a href="#">
+                        <time>12 de março de 2021</time>
+                        <strong>Creating a Monorepo with Lerna & Yarn workspaces</strong>
+                        <p>In this guide, tou will learn how to create a Monorepo to manage multiples packages with a shared build, test, and release process</p>
+                    </a>
+                    <a href="#">
+                        <time>12 de março de 2021</time>
+                        <strong>Creating a Monorepo with Lerna & Yarn workspaces</strong>
+                        <p>In this guide, tou will learn how to create a Monorepo to manage multiples packages with a shared build, test, and release process</p>
+                    </a>
+                </div>
+            </main>
+        </>
+    )
+}
