0dc5227f7a4eb79f559c4a96d5753017abd17922
Thiago Oliveira <thiagooliveira.k.on@gmail.com>
2022-07-25 03:42:03 -0300

    change import img

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { GetStaticProps } from 'next'
 
 import Head from 'next/head'
-import Image from 'next/image'
 
 import { SubscribeButton } from '../components/SubscribeButton'
 import { stripe } from '../services/stripe'
@@ -35,7 +34,7 @@ export default function Home({ product }: HomeProps) {
           <SubscribeButton />
         </section>
 
-        <Image src="/images/avatar.svg" alt="Girl coding" />
+        <img src="/images/avatar.svg" alt="Girl coding" />
       </main>
     </>
   )
